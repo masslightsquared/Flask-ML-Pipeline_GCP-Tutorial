66a24829bdbfb60ceca0672295619e110f2f3dff
masslightsquared <34934426+masslightsquared@users.noreply.github.com>
2021-02-13 13:59:58 -0600

    updated model.py

--- app_files/model.py
+++ app_files/model.py
@@ -5,18 +5,27 @@ import pickle
 
 dataset = pd.read_csv('50_Startup.csv')
 
-#Importing data set
+# Random State
+rng = np.random.RandomState(42) 
+
+# Importing data set
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,3].values
 
 from sklearn.tree import DecisionTreeRegressor
-reg=DecisionTreeRegressor(max_depth=5)
-reg.fit(X,y)
-print(reg.score)
+from sklearn.ensemble import AdaBoostRegressor
+
+# Fit regression model
+regr_1 = DecisionTreeRegressor(max_depth=5)
+regr_2 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=5),
+	n_estimators=300, random_state=rng)
+
+regr_2.fit(X,y)
+print(regr_2.score)
 
 
 #Save the model
-pickle.dump(reg,open('treemodel.pkl','wb'))
+pickle.dump(regr_2,open('treemodel.pkl','wb'))
 
 # Loading the model to compare results
 model=pickle.load(open('treemodel.pkl','rb'))
